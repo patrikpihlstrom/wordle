8c92c4e06d82e5c27c44ff226f17fcc5765dc1ef
Patrik Pihlstrom <patrik.pihlstrom@gmail.com>
2022-09-28 15:00:16 +0200

    better printing

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import random, time
 def get_words():
     with open('./words', 'r') as f:
         words = f.readlines()
+        words = list(w.lower().strip() for w in words)
         random.shuffle(words)
         return words
     raise ValueError
@@ -27,67 +28,69 @@ def get_guess(guessed: list, candidates: list, result: list, absent: list):
             if candidate in guessed:
                 continue
             found = True
-            for i in result[0]:
-                if candidate[i] != guess[i]:
-                    found = False
-                    break
-            for i in result[1]:
-                if guess[i] not in candidate:
-                    found = False
-                    break
             for c in candidate:
                 if c in absent:
                     found = False
                     break
             if found:
+                for i in result[0]:
+                    if candidate[i] != guess[i]:
+                        found = False
+                        break
+            if found:
+                for i in result[1]:
+                    if guess[i] not in candidate:
+                        found = False
+                        break
+            if found:
                 return candidate
-    return random.choice(candidates)
+    while True:
+        found = True
+        candidate = random.choice(candidates)
+        for c in candidate:
+            if c in absent:
+                found = False
+                break
+        if found:
+            return candidate
 
 
 def run():
-    while True:
-        word = get_word()
-        print(f'the word is {word}')
-        candidates = get_words()
-        guessed = []
-        result = (
-            [],
-            []
-        )
-        absent = []
+    attempts = []
+    try:
         while True:
-            time.sleep(0.5)
-            guess = get_guess(guessed, candidates, result, absent)
-            guessed.append(guess)
-            print(f'guessing {guess}')
-            if guess.lower().strip() == word.lower().strip():
-                print('Correct!')
-                break
-            elif len(guessed) == len(candidates):
-                print('Unable to guess word...')
-                break
-            else:
-                result = (
-                    [],
-                    []
-                )
-                for i in range(len(guess)):
-                    if i < len(guess) and i < len(word):
-                        if guess[i] == word[i]:
-                            result[0].append(i)
-                        elif guess[i] in word:
-                            result[1].append(i)
-                        else:
-                            absent.append(guess[i])
-                output = ''
-                for i in range(len(guess)-1):
-                    if i in result[0]:
-                        output = output + guess[i]
-                    elif i in result[1]:
-                        output = output + f'({guess[i]})'
-                    else:
-                        output = output + '_'
-                print(output)
+            word = get_word()
+            print(f'\nthe word is {word}')
+            candidates = get_words()
+            guessed = []
+            result = (
+                [],
+                []
+            )
+            absent = []
+            while True:
+                guess = get_guess(guessed, candidates, result, absent)
+                guessed.append(guess)
+                if guess.lower().strip() == word.lower().strip():
+                    print('Found the word after '+str(len(guessed))+' guesses:')
+                    print(', '.join(guessed))
+                    attempts.append(len(guessed))
+                    break
+                else:
+                    result = (
+                        [],
+                        []
+                    )
+                    for i in range(len(guess)):
+                        if i < len(guess) and i < len(word):
+                            if guess[i] == word[i]:
+                                result[0].append(i)
+                            elif guess[i] in word:
+                                result[1].append(i)
+                            else:
+                                absent.append(guess[i])
+    except KeyboardInterrupt:
+        print('average number of guesses per word (sample size: '+str(len(attempts))+' words): '+str(sum(attempts)/len(attempts)))
 
 
 if __name__ == '__main__':
